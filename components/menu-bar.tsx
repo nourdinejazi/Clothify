38ec1218a22ebcdf1ce69f93ccec48986a34d2e4
nourdine jazi <nourdinejazi2@gmail.com>
2023-07-29 14:14:32 +0100

    mainnav padding

--- components/menu-bar.tsx
+++ components/menu-bar.tsx
@@ -26,7 +26,7 @@ const MenuBar : React.FC<MenubarProps> = ({
     }
 
     return ( 
-    <div className=" absolute right-0    h-16 flex  items-center justiy-center gap-5 px-8 my-2">
+    <div className=" absolute right-0    h-16 flex  items-center justiy-center gap-5 px-4 my-2">
          <button style={{backgroundColor : '#f8f8f8'}}   className=" flex items-center justify-center rounded-full border w-[40px] h-[40px]   text-bla"> <User2 size={20} /> </button>
          <button style={{backgroundColor : '#f8f8f8'}}   className=" flex items-center justify-center rounded-full border w-[40px] h-[40px]   text-bla"> <Heart size={20} /> </button>
          <button style={{backgroundColor : '#f8f8f8'}} onClick={()=>router.push('/cart')}  className="relative flex items-center justify-center rounded-full border w-[40px] h-[40px]   text-bla"> 
