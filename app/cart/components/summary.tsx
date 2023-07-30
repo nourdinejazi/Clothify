403b80e1407354e556824c281c714b11ff4e0bb0
nourdine jazi <nourdinejazi2@gmail.com>
2023-07-30 15:56:18 +0100

    axios commit

--- app/cart/components/summary.tsx
+++ app/cart/components/summary.tsx
@@ -1,12 +1,8 @@
 "use client";
 
 import axios from "axios";
-import { useEffect,  useState } from "react";
+import {   useState } from "react";
 
-import * as z from "zod"
-
-import { useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
 
 import Currency from "@/components/currency";
 import useCart from "@/hooks/use-cart";
